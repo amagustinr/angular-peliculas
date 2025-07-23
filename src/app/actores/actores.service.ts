d8f1f80a73e545be19022cc6a7e7e2e57a396f40
amagustinr <amagustinr@gmail.com>
2025-07-23 14:41:14 -0500

    Actores

--- src/app/actores/actores.service.ts
+++ src/app/actores/actores.service.ts
@@ -1,7 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
-import { ActorCreacionDTD } from './actores';
+import { ActorCreacionDTD, ActorDTO } from './actores';
+import { PaginacionDTO } from '../compartidos/modelos/PaginacionDTO';
+import { Observable } from 'rxjs';
+import { construirQueryParams } from '../compartidos/funciones/construirQueryParams';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +15,28 @@ export class ActoresService {
   private http = inject(HttpClient);
   private urlBase = environment.apiURL + '/actores';
 
+  public obtenerPaginado(paginacion: PaginacionDTO): Observable<HttpResponse<ActorDTO[]>>{
+    let queryParams = construirQueryParams(paginacion);
+    return this.http.get<ActorDTO[]>(this.urlBase, {params: queryParams, observe: 'response'});
+  }
+
+  public obtenerPorId(id: number): Observable<ActorDTO>{
+    return this.http.get<ActorDTO>(`${this.urlBase}/${id}`);
+  }
+
+  public actualizar(id: number, actor: ActorCreacionDTD){
+    const formData = this.construirFormData(actor);
+    return this.http.put(`${this.urlBase}/${id}`, formData);
+  }
+
   public crear(actor: ActorCreacionDTD){
     const formData = this.construirFormData(actor);
     return this.http.post(this.urlBase, formData);
   }
     
+  public borrar(id:number){
+    return this.http.delete(`${this.urlBase}/${id}`);
+  }
   private construirFormData(actor: ActorCreacionDTD): FormData{
     const formData = new FormData();
 
--- src/app/actores/editar-actor/editar-actor.component.ts
+++ src/app/actores/editar-actor/editar-actor.component.ts
@@ -1,20 +1,42 @@
-import { Component, Input, numberAttribute } from '@angular/core';
+import { Component, Input, numberAttribute, OnInit, inject } from '@angular/core';
 import { ActorCreacionDTD, ActorDTO } from '../actores';
 import { FormularioActoresComponent } from "../formulario-actores/formulario-actores.component";
+import { ActoresService } from '../actores.service';
+import { Router } from '@angular/router';
+import { extraerErrores } from '../../compartidos/funciones/extraerErrores';
+import { MostrarErroresComponent } from "../../compartidos/componentes/mostrar-errores/mostrar-errores.component";
+import { CargandoComponent } from "../../compartidos/componentes/cargando/cargando.component";
 
 @Component({
   selector: 'app-editar-actor',
-  imports: [FormularioActoresComponent],
+  imports: [FormularioActoresComponent, MostrarErroresComponent, CargandoComponent],
   templateUrl: './editar-actor.component.html',
   styleUrl: './editar-actor.component.css'
 })
-export class EditarActorComponent {
+export class EditarActorComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.actoresService.obtenerPorId(this.id).subscribe(actor =>{
+      this.actor = actor;
+    })
+  }
+
   @Input({transform: numberAttribute})
   id!:number;
-
-  actor: ActorDTO = {id:1, nombre: 'Tom Holland', fechaNacimiento: new Date(1994,0,16), foto :'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Tom_Holland_at_KCA_2022.jpg/250px-Tom_Holland_at_KCA_2022.jpg'}
+  actor?: ActorDTO;
+  actoresService = inject(ActoresService);
+  route = inject(Router);
+  errores: string[] = [];
 
   guardarCambios(actor: ActorCreacionDTD){
-    console.log('editando el actor', actor);
+    this.actoresService.actualizar(this.id, actor).subscribe({
+      next: () => {
+        this.route.navigate(['/actores']);
+      },
+      error: err => {
+        const errores = extraerErrores(err);
+        this.errores = errores;
+      }
+    })
   }
 }
