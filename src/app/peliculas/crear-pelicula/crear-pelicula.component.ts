7a3befd7fd5c4eafc85fc1df8e627cdbec1b135c
amagustinr <amagustinr@gmail.com>
2025-08-12 13:47:09 -0500

    crear pelicula

--- src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
+++ src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
@@ -1,36 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PeliculaCreacionDTO } from '../peliculas';
 import { FormularioPeliculasComponent } from "../formulario-peliculas/formulario-peliculas.component";
 import { SelectorMultipleDTO } from '../../compartidos/componentes/selector-multiple/selectorMultipleModelo';
 import { actorAutoCompleteDTO } from '../../actores/actores';
+import { PeliculasService } from '../peliculas.service';
+import { Router } from '@angular/router';
+import { extraerErrores } from '../../compartidos/funciones/extraerErrores';
+import { MostrarErroresComponent } from "../../compartidos/componentes/mostrar-errores/mostrar-errores.component";
 
 @Component({
   selector: 'app-crear-pelicula',
-  imports: [FormularioPeliculasComponent],
+  imports: [FormularioPeliculasComponent, MostrarErroresComponent],
   templateUrl: './crear-pelicula.component.html',
   styleUrl: './crear-pelicula.component.css'
 })
 export class CrearPeliculaComponent {
-  
   generosSeleccionados: SelectorMultipleDTO[] = [];
-
-  generosNoSeleccionados: SelectorMultipleDTO[] = [
-    {llave: 1, valor: 'Drama'},
-    {llave: 2, valor: 'Accion'},
-    {llave: 3, valor: 'Comedia'}
-  ];
-
+  generosNoSeleccionados: SelectorMultipleDTO[] = [];
   cinesSeleccionados: SelectorMultipleDTO[] = [];
-
-  cinesNoSeleccionados: SelectorMultipleDTO[] = [
-    {llave: 1, valor: 'Cineplanet'},
-    {llave: 2, valor: 'Cinepolis'},
-    {llave: 3, valor: 'Cinemark'}
-  ];
-
+  cinesNoSeleccionados: SelectorMultipleDTO[] = [];
   actoresSeleccionados: actorAutoCompleteDTO[] = [];
+  peliculasService = inject(PeliculasService);
+  router = inject(Router);
+  errores: string[] = [];
 
-  guardarCambios(pelicula: PeliculaCreacionDTO){
-    console.log('creando pelicula', pelicula);
+  constructor() {
+    this.peliculasService.crearGet().subscribe(modelo => {
+      this.generosNoSeleccionados = modelo.generos.map(genero => {
+        return <SelectorMultipleDTO>{llave: genero.id, valor: genero.nombre};
+      });
+
+      this.cinesNoSeleccionados = modelo.cines.map(cine => {
+        return <SelectorMultipleDTO>{llave: cine.id, valor: cine.nombre};
+      });
+    });
+  }
+
+  guardarCambios(pelicula: PeliculaCreacionDTO) {
+    this.peliculasService.crear(pelicula).subscribe({
+      next: pelicula => {
+        console.log(pelicula);
+        this.router.navigate(['/']);
+      },
+      error: err => {
+        const errores = extraerErrores(err);
+        this.errores = errores;
+      }
+    });
   }
 }
